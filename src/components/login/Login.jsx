df17ad5b1a135278431ff8f2a3bf4b11b70e78c6
Akash Singh <akashsingh777567.com>
2023-06-07 04:50:30 +0530

    first commit

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div className="container">
+        <div className="content">
+            <div className="cta">
+                <img className='cta-logo-one' src="../../../public/images/cta-logo-one.png" alt="" />
+            </div>
+            <div className="bg-image"></div>
+        </div>
+    </div>
+  )
+}
+
+
+export default Login
